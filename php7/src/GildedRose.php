66303857a96ee09899abfbb766e526aa56079aaa
Pen-y-Fan <{ID}+{username}@users.noreply.github.com>
2019-08-02 19:15:23 +0100

    Refactor code and update test for Conjured item

--- php7/src/GildedRose.php
+++ php7/src/GildedRose.php
@@ -2,64 +2,106 @@
 
 namespace App;
 
-final class GildedRose {
+final class GildedRose
+{
 
     private $items = [];
+    private $item = '';
 
-    public function __construct($items) {
+    public function __construct($items)
+    {
         $this->items = $items;
     }
 
-    public function updateQuality() {
-        foreach ($this->items as $item) {
-            if ($item->name != 'Aged Brie' and $item->name != 'Backstage passes to a TAFKAL80ETC concert') {
-                if ($item->quality > 0) {
-                    if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                        $item->quality = $item->quality - 1;
-                    } else {
-                        $item->quality = 80;
-                    }
-                }
-            } else {
-                if ($item->quality < 50) {
-                    $item->quality = $item->quality + 1;
-                    if ($item->name == 'Backstage passes to a TAFKAL80ETC concert') {
-                        if ($item->sell_in < 11) {
-                            if ($item->quality < 50) {
-                                $item->quality = $item->quality + 1;
-                            }
-                        }
-                        if ($item->sell_in < 6) {
-                            if ($item->quality < 50) {
-                                $item->quality = $item->quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
-            
-            if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                $item->sell_in = $item->sell_in - 1;
-            }
-            
-            if ($item->sell_in < 0) {
-                if ($item->name != 'Aged Brie') {
-                    if ($item->name != 'Backstage passes to a TAFKAL80ETC concert') {
-                        if ($item->quality > 0) {
-                            if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                                $item->quality = $item->quality - 1;
-                            }
-                        }
-                    } else {
-                        $item->quality = $item->quality - $item->quality;
-                    }
-                } else {
-                    if ($item->quality < 50) {
-                        $item->quality = $item->quality + 1;
-                    }
-                }
+    public function updateQuality()
+    {
+        foreach ($this->items as $this->item) {
+            $this->reduceSellIn();
+
+            switch ($this->item->name) {
+                case 'Sulfuras, Hand of Ragnaros':
+                    $this->item->quality = 80;
+                    break;
+
+                case 'Aged Brie':
+                    $this->increaseQualityOfAgedBrie();
+                    break;
+
+                case 'Backstage passes to a TAFKAL80ETC concert':
+                    $this->adjustQualityOfBackstagePass();
+                    break;
+
+                case 'Conjured Mana Cake':
+                    $this->reduceQualityOfConjured();
+                    break;
+
+                default:
+                    $this->reduceQualityOfNormalItem();
+                    break;
             }
         }
     }
-}
+    
+    protected function increaseQuality()
+    {
+        if ($this->item->quality < 50) {
+            $this->item->quality ++;
+        }
+    }
 
+    protected function increaseQualityOfAgedBrie()
+    {
+        if ($this->item->sell_in < 0) {
+            $this->increaseQuality();
+        }
+        $this->increaseQuality();
+    }
+
+    protected function adjustQualityOfBackstagePass()
+    {
+        if ($this->item->sell_in < 0) {
+            return $this->item->quality = 0;
+        }
+        if ($this->item->sell_in < 5) {
+            $this->increaseQuality();
+        }
+        if ($this->item->sell_in < 10) {
+            $this->increaseQuality();
+        }
+        $this->increaseQuality();
+    }
+
+    protected function reduceQualityOfConjured()
+    {
+        if ($this->item->sell_in < 0) {
+            $this->reduceQuality();
+            $this->reduceQuality();
+        }
+        $this->reduceQuality();
+        $this->reduceQuality();
+    }
+
+    protected function reduceQualityOfNormalItem()
+    {
+        if ($this->item->sell_in < 0) {
+            $this->reduceQuality();
+        }
+        $this->reduceQuality();
+    }
+
+    protected function reduceQuality()
+    {
+        if ($this->item->quality == 0) {
+            return;
+        }
+        $this->item->quality --;
+    }
+
+    protected function reduceSellIn()
+    {
+        if ($this->item->name == 'Sulfuras, Hand of Ragnaros') {
+            return;
+        }
+        $this->item->sell_in --;
+    }
+}
